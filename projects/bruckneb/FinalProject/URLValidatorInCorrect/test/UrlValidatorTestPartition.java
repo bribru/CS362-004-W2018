cf548348018c1e9a237bad11de0a2daa68aa3bfe
bribru <bribru@users.noreply.github.com>
2018-03-16 23:33:04 -0700

    Update 20180316
    Update

---
+++ projects/bruckneb/FinalProject/URLValidatorInCorrect/test/UrlValidatorTestPartition.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import junit.framework.TestCase;
+
+//You can use this as a skeleton for your 3 different test approach
+//It is an optional to use this file, you can generate your own test file(s) to test the target function!
+// Again, it is up to you to use this file or not!
+
+public class UrlValidatorTestPartition extends TestCase {
+	
+	@BeforeEach
+	protected
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	protected
+	void tearDown() throws Exception {
+	}
+
+
+	public UrlValidatorTestPartition(String testName) {
+			super(testName);
+	}
+	
+	//This function is for First Partition testing
+	public void testYourFirstPartition() {
+		
+	}
+	
+	//This function is for Second Partition testing
+	public void testYourSecondPartition() {
+		
+	}
+	
+	//You need to create more test cases for your Partitions if you need to
+	
+}
